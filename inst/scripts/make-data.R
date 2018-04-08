b887f5f37ebea262865557e83791acf9980dabd5
mzon7 <michaelzon7@gmail.com>
2018-04-08 17:07:54 -0400

    Create make-data.R

---
+++ inst/scripts/make-data.R
@@ -0,0 +1,4 @@
+#This package contains multiple pancreas cancer datasets that have been downloaded from various resources and turned into
+#expressionSet objects. The details of how the authors normalized the data can be found in the experiment data section of 
+#the objects. Additionally, the location the data was obtained from can be found in the url variables of the experiment data
+#portion of each eset.
