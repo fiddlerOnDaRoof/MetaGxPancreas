a92da60022057e54bc3ab69649324ecde423a070
mzon7 <michaelzon7@gmail.com>
2018-07-16 10:25:59 -0400

    Update make-metadata.R

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -3,13 +3,13 @@
 
 infoString = c("BADEA", "BALAGURANATH" ,"CHEN", "COLLISON", "GRUTZMANN", "ICGCSEQ", "ICGCMICRO",
                "KIRBY", "OUH", "PCSI", "PEI", "TCGA",
-               "UNC", "WINTER", "ZHANG", "HAMIDI","YANG", "LUNARDI", "JANKY", "BAUER", "HAIDER", "VANDENBROECK")
+               "UNC", "WINTER", "ZHANG", "HAMIDI","YANG", "LUNARDI", "JANKY", "BAUER", "HAIDER")
 
 
 main.data <- data.frame(
   Title = c("BADEA", "BALAGURANATH" ,"CHEN", "COLLISON", "GRUTZMANN", "ICGCSEQ", "ICGCMICRO",
             "KIRBY", "OUH", "PCSI", "PEI", "TCGA",
-            "UNC", "WINTER", "ZHANG", "HAMIDI","YANG", "LUNARDI", "JANKY", "BAUER", "HAIDER", "VANDENBROECK"
+            "UNC", "WINTER", "ZHANG", "HAMIDI","YANG", "LUNARDI", "JANKY", "BAUER", "HAIDER"
   ),
   Description = paste(
     "Pancreas cancer  gene expression data ",
@@ -34,8 +34,7 @@ main.data <- data.frame(
       "used in, Lunardi S et al, 2014, Oncotarget",
       "used in, Janky et al, BMC Cancer 2016",
       "used in, Bauer et al, 2016, Gastroenterology",
-      "used in, haider et al, Genome medicine, 2014",
-      "used in, Van den Broeck A et al. J Exp Clin Cancer Res 2012"
+      "used in, haider et al, Genome medicine, 2014"
     )
   ),
   RDataPath = c(
@@ -45,14 +44,13 @@ main.data <- data.frame(
     "MetaGxPancreas/PCSI.rda", "MetaGxPancreas/PEI.rda", "MetaGxPancreas/TCGA.rda",
     "MetaGxPancreas/UNC.rda", "MetaGxPancreas/WINTER.rda", "MetaGxPancreas/ZHANG.rda",
     "MetaGxPancreas/HAMIDI","MetaGxPancreas/YANG", "MetaGxPancreas/LUNARDI", 
-    "MetaGxPancreas/JANKY", "MetaGxPancreas/BAUER", "MetaGxPancreas/HAIDER", 
-    "MetaGxPancreas/VANDENBROECK"
+    "MetaGxPancreas/JANKY", "MetaGxPancreas/BAUER", "MetaGxPancreas/HAIDER"
   ),
   BiocVersion="3.7",
   Genome=c("Affymetrix Human Genome U133 Plus 2.0 Array", "Human 1A Microarray G4110A-G4110B", "Affymetrix,Rosetta-Merck RSTA Custom 2.0", "Affymetrix, array U133 Plus 2.0","Affymetrix GeneChip Human Genome HG-U133B",
            "Illumina HumanHT-12 V4.0 expression beadchip","Illumina HiSeq 2000-2500","Illumina, RNA seqencing HiSeq","Agilent-028004 SurePrint G3 Human GE 8x60K Microarray","Illumina HiSeq 2000-2500","Affymetrix Human Genome U133 Plus 2.0 Array","IlluminaHiSeq_RNASeqV2","Agilent-014850 Whole Human Genome Microarray 4x44K G4112F Agilent-014850 Whole Human Genome Microarray 4x44K G4112F ",
            "A-AFFY-44-Affymetrix GeneChip Human Genome U133 Plus 2.0 [HG-U133_Plus_2]","Affymetrix GeneChip Human Gene 1.0 ST arrays", "Agilent-012097 Human 1A Microarray (V2) G4110B", "Affymetrix GeneChip Human Gene 1.0 ST", " Agilent-014850 Whole Human Genome Microarray 4x44K G4112F",
-           "Affymetrix Human Genome U219 Array", "Illumina human WG6 Expression BeadChip", "Affymetrix Human Exon 1.0 ST Array", "Affymetrix, array U133 Plus 2.0"
+           "Affymetrix Human Genome U219 Array", "Illumina human WG6 Expression BeadChip", "Affymetrix Human Exon 1.0 ST Array"
   ),
   SourceType="RData",
   SourceUrl=c("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=gse15471", "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE11838",
@@ -65,12 +63,12 @@ main.data <- data.frame(
               "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE28735", "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE77858",
               "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE62452", "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE55643",
               "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE62165", "https://www.ebi.ac.uk/arrayexpress/experiments/E-MTAB-1791/",
-              "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE56560", "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE42952"),
+              "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE56560"),
   SourceVersion="July 09 2018",
   Species="Homo sapien",
   TaxonomyId=9606,
   Coordinate_1_based=FALSE,
-  DataProvider=c(rep("GEO", 4), "Array Express", rep("ICGC", 2), rep("GEO", 2), "Private", "GEO", "GDC", "GEO", "Array Express", "GEO", rep("GEO", 4), "Array Express", "GEO", "GEO"),
+  DataProvider=c(rep("GEO", 4), "Array Express", rep("ICGC", 2), rep("GEO", 2), "Private", "GEO", "GDC", "GEO", "Array Express", "GEO", rep("GEO", 4), "Array Express", "GEO"),
   Maintainer="Michael Zon <michaelzon7@gmail.com>",
   RDataClass="ExpressionSet",
   DispatchClass="Rda",
